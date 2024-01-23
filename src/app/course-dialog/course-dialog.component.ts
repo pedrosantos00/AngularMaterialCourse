95fae796104e3e490723d5810eec9e48d236ce44
Pedro <pedro@cardexchangeid.com>
2024-01-23 16:31:44 +0000

    This is a new commit for what I originally planned to be amended

---
+++ src/app/course-dialog/course-dialog.component.ts
@@ -0,0 +1,40 @@
+import {Component, Inject, OnInit, ViewEncapsulation} from '@angular/core';
+import {MAT_DIALOG_DATA,  MatDialog,  MatDialogConfig,  MatDialogRef} from '@angular/material/dialog';
+import {Course} from "../model/course";
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
+import * as moment from 'moment';
+
+@Component({
+    selector: 'course-dialog',
+    templateUrl: './course-dialog.component.html',
+    styleUrls: ['./course-dialog.component.css']
+})
+export class CourseDialogComponent implements OnInit {
+
+  description : string;
+  form = this.fb.group({
+    description: ['',Validators.required],
+    category:["BEGINNER", Validators.required],
+    releaseAt : [new Date(),Validators.required],
+    longDescription: ["",Validators.required]  });
+
+    constructor(private fb: FormBuilder) {
+
+
+    }
+
+
+    close(){
+
+    }
+
+    save(){
+
+    }
+
+    ngOnInit() {
+
+    }
+
+}
+
